61af0b06171f29ad0deb4c2df7c1d601afd4b868
shubham0473 <shubham.agrawal.0473@gmail.com>
2015-12-21 05:07:00 +0530

    add user complete

--- app.py
+++ app.py
@@ -1,7 +1,36 @@
-from flask import Flask, render_template, request
 import json
+from flask import Flask, make_response, request, url_for, jsonify, render_template, request
+import MySQLdb
+from flask.ext.httpauth import HTTPBasicAuth
+import os
+
+
+UPLOAD_FOLDER = '/home/shubham/Desktop/web_development/tutplus/data/user_dp/'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+auth = HTTPBasicAuth()
+
+@auth.get_password
+def get_password(username):
+    if username == 'miguel':
+        return 'python'
+    return None
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify( { 'error': 'Unauthorized access' } ), 403)
+    # return 403 instead of 401 to prevent browsers from displaying the default auth dialog
+    
+@app.errorhandler(400)
+def not_found(error):
+    return make_response(jsonify( { 'error': 'Bad request' } ), 400)
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify( { 'error': 'Not found' } ), 404)
+
 
 @app.route("/")
 def main():
@@ -11,22 +40,44 @@ def main():
 def showSignUp():
     return render_template('signup.html')
 
-@app.route('/showCompetition1')
-def showCompetition1():
-    return render_template('competition_1.html')
-
-@app.route('/signUp',methods=['POST'])
-def signUp():
-    # read the posted values from the UI
-    _name = request.form['inputName']
-    _email = request.form['inputEmail']
-    _password = request.form['inputPassword']
-
-    if _name and _email and _password:
-    	return json.dumps({'html':'<span>All fields good !!</span>'})
-
-    else:
-    	return json.dumps({'html':'<span>Enter the required fields</span>'})
+@app.route('/showLogin')
+def showLogin():
+    return render_template('login.html')
     
+# connect
+db=MySQLdb.connect(host="localhost", user="root", passwd="shubham123",
+    db="MYGIG")
+
+cursor = db.cursor()
+
+@app.route('/user', methods = ['POST'])
+def add_user():
+    if not request.form:
+        print("error")
+        return jsonify({'error': 'bad request'})
+    query = """INSERT INTO `db_user` 
+    (name, email, password, link_to_dp, type_flag) 
+    VALUES 
+    ("{name_}", "{email_}", "{password_}", "{link_}", "{flag_}");"""
+    name = request.form['name']
+    email = request.form['email']
+    password = request.form['password']
+    link = ""
+    # file = request.files['file']
+    # if file and allowed_file(file.filename):
+    #     filename = secure_filename(file.filename)
+    #     file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    #     link = url_for('uploaded_file',filename=filename)
+    # print link
+    flag = request.form['flag']
+    cursor.execute(query.format(name_=name,email_= email,password_= password,link_= link,flag_= flag))
+    db.commit()
+    return jsonify({'name' : name}), 200
+
+# @app.route('/user/<int:user_id>' methods = ['GET'])
+# @auth.login_required
+# def get_profile(user_id):
+
+
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/signup.js
+++ static/js/signup.js
@@ -1,16 +1,65 @@
-$(function() {
-    $('#btnSignUp').click(function() {
- 
-        $.ajax({
-            url: '/signUp',
-            data: $('form').serialize(),
-            type: 'POST',
-            success: function(response) {
-                console.log(response);
-            },
-            error: function(error) {
-                console.log(error);
-            }
-        });
+
+$('#btnSignUp').click(function() {
+
+    // var myfile = document.getElementById('image').files[0];
+    // var fr = new FileReader();
+    // var user;
+    // fr.onload = function(event) {
+    //     var data = event.target.result.replace("data:"+ myfile.type +";base64,", '');
+
+    // }
+    // fr.readAsDataURL(myfile);
+    user = {
+        name : $("#inputName").val(),
+        email : $("#inputEmail").val(),
+        password : $("#inputPassword").val(),
+        flag : "1"
+    }
+
+    $("#status").html('waiting...');
+
+    $.ajax({
+        url: 'http://127.0.0.1:5000/user',
+        dataType: 'json',
+        type: 'POST',
+        success: function(response) {
+            console.log("user added");
+        },
+        error: function(error) {
+            console.log(error);
+        },
+        data: user
     });
 });
+
+function getBase64Image(imgElem) {
+// imgElem must be on the same server otherwise a cross-origin error will be thrown "SECURITY_ERR: DOM Exception 18"
+var canvas = document.createElement("canvas");
+canvas.width = imgElem.clientWidth;
+canvas.height = imgElem.clientHeight;
+var ctx = canvas.getContext("2d");
+ctx.drawImage(imgElem, 0, 0);
+var dataURL = canvas.toDataURL("image/png");
+return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+}
+
+
+// function uploadFile() {
+//     blobFile = $('#filechooser')[0].files[0]);
+//     var fd = new FormData();
+//     fd.append("fileToUpload", blobFile);
+
+//     $.ajax({
+//        url: "upload.php",
+//        type: "POST",
+//        data: fd,
+//        processData: false,
+//        contentType: false,
+//        success: function(response) {
+//            // .. do something
+//        },
+//        error: function(jqXHR, textStatus, errorMessage) {
+//            console.log(errorMessage); // Optional
+//        }
+//     });
+// }
