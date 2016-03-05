91e76f318e87ede9e687d0bf643ab47314a1209e
Vadde Santosh <vaddesantosh96@gmail.com>
2016-03-05 07:16:32 +0530

    Barno ki chutiyaap , Gm people

--- mysite/maggulater/models.py
+++ mysite/maggulater/models.py
@@ -22,18 +22,17 @@ class MyUser(models.Model):
 	def set_password(self, password):
 		self.password = password
 	def serialize(self):
-        """Return object data in easily serializeable format"""
-        return {
-            'user_id' : self.user_id,
-            'name' : self.name,
-            'email' : self.email,
-            'password' : self.password,
-            'link_to_dp' : self.link_to_dp,
-            'type_flag' : self.type_flag,
-            'dob' : self.dob
-            # This is an example how to deal with Many2Many relations
-            #    'many2many'  : self.serialize_many2many
-        }
+		return {
+			'user_id' : self.user_id,
+			'name' : self.name,
+			'email' : self.email,
+			'password' : self.password,
+			'link_to_dp' : self.link_to_dp,
+			'type_flag' : self.type_flag,
+			'dob' : self.dob
+			# This is an example how to deal with Many2Many relations
+			#	'many2many'  : self.serialize_many2many
+			}
 
 #Student
 class Student(models.Model):
@@ -41,7 +40,7 @@ class Student(models.Model):
 
 #Faculty
 class Faculty(models.Model):
-    Faculty_Id = models.ForeignKey(MyUser,on_delete=models.CASCADE, primary_key = True)
+	Faculty_Id = models.ForeignKey(MyUser,on_delete=models.CASCADE, primary_key = True)
 
 #Admin
 class Admin(models.Model):
@@ -49,108 +48,108 @@ class Admin(models.Model):
 
 #Course
 class Course(models.Model):
-    course_id = models.AutoField(primary_key = True)
-    faculty = models.ForeignKey(Faculty ,on_delete=models.CASCADE)
-    course_name = models.CharField(max_length = 30)
-    prereq = models.IntegerField(default = -1)
-    syllabus = models.CharField(max_length = 500)
-    approved = models.IntegerField(default = 0 ) # 1 after admin approves the course
-    def approve(self):
-        self.approve = 1
+	course_id = models.AutoField(primary_key = True)
+	faculty = models.ForeignKey(Faculty ,on_delete=models.CASCADE)
+	course_name = models.CharField(max_length = 30)
+	prereq = models.IntegerField(default = -1)
+	syllabus = models.CharField(max_length = 500)
+	approved = models.IntegerField(default = 0 ) # 1 after admin approves the course
+	def approve(self):
+		self.approve = 1
 
-    def setSyllabus(self, syl):
-        self.syllabus = syl
-    def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
-           'course_id' : self.course_id,
-           'faculty' : self.faculty,
-           'course_name' : self.course_name,
-           'prereq' : self.prereq,
-           'syllabus' : self.syllabus,
-           'approved' : self.approved
-           # This is an example how to deal with Many2Many relations
-        #    'many2many'  : self.serialize_many2many
-       }
+	def setSyllabus(self, syl):
+		self.syllabus = syl
+	def serialize(self):
+		"""Return object data in easily serializeable format"""
+		return {
+			'course_id' : self.course_id,
+			'faculty' : self.faculty,
+			'course_name' : self.course_name,
+			'prereq' : self.prereq,
+			'syllabus' : self.syllabus,
+			'approved' : self.approved
+			# This is an example how to deal with Many2Many relations
+		#	'many2many'  : self.serialize_many2many
+		}
 #Lecture
 class Lecture(models.Model):
-    Lecture_Id = models.AutoField(primary_key = True)
-    Course_Id = models.ForeignKey(Course, on_delete =models.CASCADE)
-    Notes = models.TextField
-    Date_Time = models.DateTimeField
-    Topic = models.TextField
-    Link = models.CharField(max_length = 100)
+	Lecture_Id = models.AutoField(primary_key = True)
+	Course_Id = models.ForeignKey(Course, on_delete =models.CASCADE)
+	Notes = models.TextField
+	Date_Time = models.DateTimeField
+	Topic = models.TextField
+	Link = models.CharField(max_length = 100)
 
-    def setNotes(self , notes):
-        self.Notes = notes 
+	def setNotes(self , notes):
+		self.Notes = notes 
 
-    def setLink(self, link):
-        self.Link = link 
-    def serialize(self):
-        return {
-            'Lecture_Id' : self.Lecture_Id,
-            'Course_Id' : self.Course_Id ,
-            'Notes' : self.Notes ,
-            'Date_Time' : self.Date_Time,
-            'Topic' : self.Topic,
-            'Link' : self.Link,
-        }
+	def setLink(self, link):
+		self.Link = link 
+	def serialize(self):
+		return {
+			'Lecture_Id' : self.Lecture_Id,
+			'Course_Id' : self.Course_Id ,
+			'Notes' : self.Notes ,
+			'Date_Time' : self.Date_Time,
+			'Topic' : self.Topic,
+			'Link' : self.Link,
+		}
 #Test
 class Test(models.Model):
-    Test_Id = models.AutoField(primary_key = True)
-    Lecture_Id = models.ForeignKey(Lecture, on_delete = models.CASCADE)
-    Questions = models.TextField
-    Answer_Sheet = models.TextField
-    def serialize(self):
-        return {
-            'Test_Id' : self.Test_Id,
-            'Lecture_Id' : self.Lecture_Id,
-            'Questions' : self.Questions,
-            'Answer_Sheet' : self.Answer_Sheet
-        }
+	Test_Id = models.AutoField(primary_key = True)
+	Lecture_Id = models.ForeignKey(Lecture, on_delete = models.CASCADE)
+	Questions = models.TextField
+	Answer_Sheet = models.TextField
+	def serialize(self):
+		return {
+			'Test_Id' : self.Test_Id,
+			'Lecture_Id' : self.Lecture_Id,
+			'Questions' : self.Questions,
+			'Answer_Sheet' : self.Answer_Sheet
+		}
 #Performance 
 class Performance_Sheet(models.Model):
-    Student_Id = models.ForeignKey(Student, unique = True , on_delete = models.CASCADE)
-    Test_Id = models.ForeignKey(Test ,  unique = True, on_delete = models.CASCADE)
-    Marks_Obtained = models.FloatField
-    Marks_Total = models.FloatField
-    def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
-           'Student_Id' : self.Student_Id,
-           'Test_Id' : self.Test_Id,
-           'Marks_Obtained' : self.Marks_Obtained,
-           'Marks_Total' : self.Marks_Total
-           # This is an example how to deal with Many2Many relations
-        #    'many2many'  : self.serialize_many2many
-       }
+	Student_Id = models.ForeignKey(Student, unique = True , on_delete = models.CASCADE)
+	Test_Id = models.ForeignKey(Test ,  unique = True, on_delete = models.CASCADE)
+	Marks_Obtained = models.FloatField
+	Marks_Total = models.FloatField
+	def serialize(self):
+		"""Return object data in easily serializeable format"""
+		return {
+			'Student_Id' : self.Student_Id,
+			'Test_Id' : self.Test_Id,
+			'Marks_Obtained' : self.Marks_Obtained,
+			'Marks_Total' : self.Marks_Total
+			# This is an example how to deal with Many2Many relations
+		#	'many2many'  : self.serialize_many2many
+		}
 # Enrolls relationship
 class Enrolls(models.Model):
-    enrolls_id = models.AutoField(primary_key = True)
-    student_id = models.ForeignKey(Student, on_delete = models.CASCADE)
-    course_id = models.ForeignKey(Course, on_delete = models.CASCADE)
-    def serialize(self):
-        """Return object data in easily serializeable format"""
-        return {
-            'enrolls_id' : self.enrolls_id,
-            'student_id' : self.student_id,
-            'course_id' : self.course_id
-            # This is an example how to deal with Many2Many relations
-            #    'many2many'  : self.serialize_many2many
-        }
+	enrolls_id = models.AutoField(primary_key = True)
+	student_id = models.ForeignKey(Student, on_delete = models.CASCADE)
+	course_id = models.ForeignKey(Course, on_delete = models.CASCADE)
+	def serialize(self):
+		"""Return object data in easily serializeable format"""
+		return {
+			'enrolls_id' : self.enrolls_id,
+			'student_id' : self.student_id,
+			'course_id' : self.course_id
+			# This is an example how to deal with Many2Many relations
+			#	'many2many'  : self.serialize_many2many
+		}
 # Notice Table
 class Notice(models.Model):
-    notice_id = models.IntegerField( primary_key = True)
-    timestamp = models.DateTimeField
-    message = models.CharField(max_length = 500)
-    c_id = models.ForeignKey(Course, on_delete = models.CASCADE)
-    def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
-           'notice_id' : self.notice_id,
-           'timestamp' : self.timestamp,
-           'message' : self.message,
-           'c_id' : self.c_id
-           # This is an example how to deal with Many2Many relations
-        #    'many2many'  : self.serialize_many2many
-       }
+	notice_id = models.IntegerField( primary_key = True)
+	timestamp = models.DateTimeField
+	message = models.CharField(max_length = 500)
+	c_id = models.ForeignKey(Course, on_delete = models.CASCADE)
+	def serialize(self):
+		"""Return object data in easily serializeable format"""
+		return {
+			'notice_id' : self.notice_id,
+			'timestamp' : self.timestamp,
+			'message' : self.message,
+			'c_id' : self.c_id
+			# This is an example how to deal with Many2Many relations
+		#	'many2many'  : self.serialize_many2many
+		}
