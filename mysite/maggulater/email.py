935f2078c9f04bf67671899ffaaf28e24e8e6cd2
Divyansh Gupta <divyanshgupta95@gmail.com>
2016-03-10 13:27:48 +0530

    Email integrated in course home

--- mysite/maggulater/email.py
+++ mysite/maggulater/email.py
@@ -1,21 +1,46 @@
 import sys
 import telnetlib
+import unicodedata
 
-HOST = "10.3.100.244"
-PORT = 25
+def remove_non_ascii_1(text):
+	return ''.join(i for i in text if ord(i)<128)
 
-tn = telnetlib.Telnet(HOST, PORT)
+def sendEmail(sender, recipients, subject, body):
+	HOST = "10.3.100.244"
+	PORT = 25
 
-# print tn.read_until("Postfix")
-print tn.read_eager()
-tn.write("HELO iitkgp.ernet.in\n")
-print tn.read_until("ac.in")
-tn.write("MAIL FROM: ppc@cse.iitkgp.ernet.in\n")
-print tn.read_until("Ok")
-tn.write("RCPT TO: any_user@gmail.com\n")
-print tn.read_until("Ok")
-tn.write("DATA\n")
-print tn.read_until("<LF>")
-tn.write("Subject: This is a python test \r\nIf this works we are almost done\r\n.\r\n")
-print tn.read_all()
-telnet.close()
+	# reload(sys)
+	# sys.setdefaultencoding('utf8')
+
+	tn = telnetlib.Telnet(HOST, PORT)
+
+	# print tn.read_until("Postfix")
+	print tn.read_eager()
+
+	tn.write("HELO iitkgp.ernet.in\n")
+	print tn.read_until("ac.in")
+
+	text = "MAIL FROM: " + sender + "\n"
+	# print text
+	text = unicodedata.normalize('NFKD',text).encode('ascii', 'ignore')
+	# text = remove_non_ascii_1(text)
+	# print text
+	tn.write(text)
+	print tn.read_until("Ok")
+
+	for recipient in recipients:
+		text = "RCPT TO: " + recipient + "\n"
+		text = unicodedata.normalize('NFKD',text).encode('ascii', 'ignore')
+		tn.write(text)
+		print tn.read_until("Ok")
+
+	tn.write("DATA\r\n")
+	print tn.read_until("<LF>")
+
+	text = "Subject: " + subject + " \r\n" + body +"\r\n.\r\n"
+	text = unicodedata.normalize('NFKD',text).encode('ascii', 'ignore')
+	tn.write(text)
+	print tn.read_all()
+
+	telnet.close()
+	return
---
+++ mysite/maggulater/email_test.py
@@ -0,0 +1,4 @@
+
+from .email import sendEmail
+
+sendEmail('divyanshgupta95@gmail.com', ['divyanshgupta95@gmail.com'], "hello", "hi")
